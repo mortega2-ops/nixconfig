eeaab99962851c25ccda0ee1010be0e17e9f4ad8
mortega2-ops <rydin303@hotmail.com>
2025-09-24 20:21:53 -0700

    ledger change

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -193,10 +193,14 @@
 
   # List services that you want to enable:
 
-  services.udev.extraRules = ''
-    # Ledger Nano S Plus (2c97:5011)
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="5011", MODE="0664", GROUP="users", TAG+="uaccess"
-    '';
+  services.udev.packages = [
+    pkgs.ledger-udev-rules
+  ];
+
+  # services.udev.extraRules = ''
+  #   # Ledger Nano S Plus (2c97:5011)
+  #   SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="5011", MODE="0664", GROUP="users", TAG+="uaccess"
+  # '';
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
