2180a09c69a3d88864c9750531400ed029a9761c
mortega2-ops <rydin303@hotmail.com>
2025-09-27 09:42:52 -0700

    update ghostty config

--- hosts/default/features/ghostty.nix
+++ hosts/default/features/ghostty.nix
@@ -2,5 +2,16 @@
 {
   programs.ghostty = {
     enable = true;
+    theme = "Firewatch";
+    settings = {
+      font-size = 16;
+      font-family = "JetBrainsMono Nerd Font";
+      
+      unfocused-split-opacity = 0.96;
+
+      # Disables ligatures
+      font-feature = ["-liga" "-dlig" "-calt"];
+
+    }
   };
 }
