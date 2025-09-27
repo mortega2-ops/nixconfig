242b149ecc623ad9fd11a20a1f315a8aae44a602
mortega2-ops <rydin303@hotmail.com>
2025-09-26 20:52:23 -0700

    testing config

--- hosts/default/features/ghostty.nix
+++ hosts/default/features/ghostty.nix
@@ -4,9 +4,9 @@
 
   programs.ghostty = {
     enable = true;
-    settings = {
-      foreground = "#${config.colorScheme.palette.base05}";
-      background = "#${config.colorScheme.palette.base00}";
-    };
+    # settings = {
+    #   foreground = "#${config.colorScheme.palette.base05}";
+    #   background = "#${config.colorScheme.palette.base00}";
+    # };
   };
 }
