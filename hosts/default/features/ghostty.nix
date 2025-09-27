91bd01d904e3ca64fe48d224726e899e6d28e5e9
mortega2-ops <rydin303@hotmail.com>
2025-09-26 23:34:23 -0700

    testing config

--- hosts/default/features/ghostty.nix
+++ hosts/default/features/ghostty.nix
@@ -4,9 +4,6 @@
 
   programs.ghostty = {
     enable = true;
-    # settings = {
-    #   foreground = "#${config.colorScheme.palette.base05}";
-    #   background = "#${config.colorScheme.palette.base00}";
-    # };
+    theme = "GruvboxDark";
   };
 }
